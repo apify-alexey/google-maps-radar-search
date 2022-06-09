329294ea5b56e07ea53022eaa33deecb2b492c7f
apify-alexey <alexey.udovydchenko@apify.com>
2022-06-09 15:39:42 +0700

    beta

---
+++ main.js
@@ -0,0 +1,53 @@
+const Apify = require('apify');
+
+const { createApiCallsByCategory, handleApiResults } = require('./src/routes');
+
+const { utils: { log } } = Apify;
+
+Apify.main(async () => {
+    const input = await Apify.getInput();
+    // accept api keys from process.env for custom actor builds
+    input.apiKey = input.apiKey || process.env.apiKey || process.env.APIKEY;
+    // default radius is 1000 meters
+    input.radiusMeters = input.radiusMeters || 1000;
+
+    const { apiKey, latitude, longitude, radiusMeters, maxResults, proxy, debugLog } = input;
+
+    if (!(apiKey && latitude && longitude)) {
+        log.info('REQUIRED apiKey, latitude, longitude', input);
+        throw new Error('BROKEN-INPUT');
+    }
+
+    if (debugLog) {
+        log.setLevel(log.LEVELS.DEBUG);
+    }
+
+    const state = await Apify.getValue('STATE') || {
+        places: [], // saved places to checkup for duplicates
+        finishedCategory: [], // add category name when end of results is reached
+    };
+    const persistState = async () => { await Apify.setValue('STATE', state); };
+    Apify.events.on('persistState', persistState);
+
+    const requestList = await Apify.openRequestList('start-urls', createApiCallsByCategory(input));
+    const requestQueue = await Apify.openRequestQueue();
+    const proxyConfiguration = await Apify.createProxyConfiguration(proxy);
+
+    const crawler = new Apify.CheerioCrawler({
+        requestList,
+        requestQueue,
+        proxyConfiguration,
+        // must follow API Rate limit (100 requests per second)
+        // implemented as 1 maxConcurrency with sleep(msDelayForApiCalls)
+        maxConcurrency: 1,
+        maxRequestRetries: 1,
+        maxRequestsPerCrawl: maxResults ? Math.floor(maxResults / 20) : undefined,
+        handlePageFunction: async (context) => {
+            return handleApiResults(context, state);
+        },
+    });
+
+    log.info(`Getting places in ${radiusMeters} meters around coordinates ${latitude}, ${longitude}`);
+    await crawler.run();
+    log.info(`Crawl finished with full results for ${state.finishedCategory.length} categories`);
+});
---
+++ src/routes.js
@@ -0,0 +1,79 @@
+const Apify = require('apify');
+const turf = require('@turf/turf');
+
+const { placeTypes, msDelayForApiCalls } = require('./consts');
+
+const { utils: { log, sleep } } = Apify;
+
+// create nearbysearch calls per category rankby distance
+exports.createApiCallsByCategory = ({ apiKey, latitude, longitude, radiusMeters, categories }) => {
+    const apiRequests = [];
+    const baseApi = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location=${latitude}%2C${longitude}&rankby=distance&key=${apiKey}`;
+    // if input.categories array not specified use all possible placeTypes
+    for (const category of categories?.length ? categories : placeTypes) {
+        apiRequests.push({
+            url: `${baseApi}&type=${category}`,
+            userData: {
+                category,
+                latitude,
+                longitude,
+                radiusMeters,
+            },
+        });
+    }
+    return apiRequests;
+};
+
+// process results from nearbysearch
+exports.handleApiResults = async ({ request, json, crawler }, { places, finishedCategory }) => {
+    const { url, userData } = request;
+    const { category, latitude, longitude, radiusMeters } = userData;
+    if (!(json && latitude && longitude && radiusMeters)) {
+        log.error(`Invalid request ${url}`, userData);
+        throw new Error('INVALID-REQUEST');
+    }
+
+    await sleep(msDelayForApiCalls);
+
+    if (!json?.results?.length) {
+        log.error(`NO-PLACES from ${url}`);
+        return;
+    }
+
+    // save unique results to dataset
+    const saveNewPlaces = [];
+    for (const place of json.results) {
+        const checkPlace = places.find((x) => x.place_id === place.place_id);
+        if (!checkPlace) {
+            places.push(place);
+            saveNewPlaces.push(place);
+        } else {
+            log.debug(`Skipped existing place ${place.place_id}`);
+        }
+    }
+    if (saveNewPlaces?.length) {
+        await Apify.pushData(saveNewPlaces);
+    }
+
+    const lastPlaceLocation = json.results[json.results.length - 1].geometry.location;
+    const coordsFrom = turf.point([latitude, longitude]);
+    const coordsTo = turf.point([lastPlaceLocation.lat, lastPlaceLocation.lng]);
+    const distanceMeters = turf.distance(coordsFrom, coordsTo) * 1000;
+    if (distanceMeters < radiusMeters) {
+        // if radius not reached continue to next page of results
+        if (json?.next_page_token) {
+            log.debug(`Next page for category ${category}`);
+            const nextUrl = new URL(url);
+            nextUrl.searchParams.set('pagetoken', json.next_page_token);
+            await crawler.requestQueue.addRequest({
+                url: nextUrl.toString(),
+                userData,
+            });
+        } else {
+            log.warning(`[CATEGORY]: ${category} have more than 60 results`);
+        }
+    } else {
+        log.info(`[CATEGORY]: ${category} - reached end of results at ${distanceMeters} meters`);
+        finishedCategory.push(category);
+    }
+};
